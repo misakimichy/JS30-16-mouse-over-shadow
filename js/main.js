ee70d47c2c8b3eb37721d0e0bce1719cfde5d8b8
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-18 13:08:44 -0700

    Add single shadow and multiple shadows.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 (function(){
     const hero = document.querySelector('.hero');
-    const text= document.querySelector('h1');
+    const text = document.querySelector('h1');
+    const walk = 100;
 
     const handleShadow = (e) => {
         const { offsetWidth: width, offsetHeight: height } = hero;
@@ -12,7 +13,21 @@
             x = x + e.target.offsetLeft;
             y = y + e.target.offsetTop;
         }
-        console.log(x, y);
+        
+        // Handle how far the shadow goes (range will be between -50 to 50)
+        const xWalk = Math.round((x / width * walk)) - (walk / 2);
+        const yWalk = Math.round((y / height * walk)) - (walk / 2);
+        
+        // Single shadow
+        text.style.textShadow = `${xWalk}px ${yWalk}px 0 rgba(0, 0, 0, 0.3)`;
+        
+        // Multiple shadows
+        // text.style.textShadow = `
+        //     ${xWalk}px ${yWalk}px 0 rgba(255, 0, 0, 0.4),
+        //     ${xWalk * -1}px ${yWalk}px 0 rgba(102, 51, 153, 0.3),
+        //     ${yWalk}px ${xWalk * -1}px 0 rgba(255, 165, 0, 0.3),
+        //     ${yWalk * -1}px ${xWalk}px 0 rgba(0, 0, 255, 0.3)
+        // `;
     };
 
     hero.addEventListener('mousemove', handleShadow);
